41e5c9514f47130acd0179894afd0805482b26f5
ManojRameh <Manojramesh987@gmail.com>
2024-12-20 11:24:50 +0530

    I love git

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,8 +21,8 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('get_students/', views.get_students, name='get_students'),
     path('add_student/', views.add_student, name='add_student'),
+    path('get_students/', views.get_students, name='get_students'),
     path('update_student/<int:student_id>/', views.update_student, name='update_student'),
     path('delete_student/<int:student_id>/', views.delete_student, name='delete_student'),
 
